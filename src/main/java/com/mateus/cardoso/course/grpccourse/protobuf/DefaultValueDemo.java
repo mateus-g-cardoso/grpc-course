640201c5e30caf2a73cfeb38411106e2790c0cbd
Mateus Cardoso <mateus.cardoso@picpay.com>
2023-05-25 11:52:54 -0300

    class 28 Proto - Default Values

---
+++ src/main/java/com/mateus/cardoso/course/grpccourse/protobuf/DefaultValueDemo.java
@@ -0,0 +1,8 @@
+package com.mateus.cardoso.course.grpccourse.protobuf;
+
+public class DefaultValueDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/mateus/cardoso/course/grpccourse/protobuf/MapDemo.java
@@ -0,0 +1,39 @@
+package com.mateus.cardoso.course.grpccourse.protobuf;
+
+import com.mateus.cardoso.course.grpccourse.grpc.lib.BodyStyle;
+import com.mateus.cardoso.course.grpccourse.grpc.lib.Car;
+import com.mateus.cardoso.course.grpccourse.grpc.lib.Dealer;
+
+import java.util.Map;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+        var accord = Car.newBuilder()
+            .setMake("Honda")
+            .setModel("Accord")
+            .setBodyStyle(BodyStyle.COUPE)
+            .setYear(2020)
+            .build();
+        var civic = Car.newBuilder()
+            .setMake("Honda")
+            .setModel("Civic")
+            .setBodyStyle(BodyStyle.SEDAN)
+            .setYear(2005)
+            .build();
+        var cars = Map.of(
+            2005, civic,
+            2020, accord
+              );
+
+        var dealer = Dealer.newBuilder()
+//            .putModel(2005, civic)
+//            .putModel(2020, accord)
+            .putAllModel(cars)
+            .build();
+
+        System.out.println(
+            dealer.getModelOrThrow(2020).getBodyStyle()
+                          );
+    }
+}
