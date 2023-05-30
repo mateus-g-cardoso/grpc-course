84ca43fd20977e0b5997d530eb1e04f0dd392ec6
Mateus Cardoso <mateus.cardoso@picpay.com>
2023-05-30 14:52:30 -0300

    class 44. Unary - BankService - getBalance - Impl

---
+++ grpc-intro/src/main/java/com/mateus/cardoso/course/grpc/grpcintro/server/BankService.java
@@ -0,0 +1,21 @@
+package com.mateus.cardoso.course.grpc.grpcintro.server;
+
+import com.mateus.cardoso.course.grpc.grpcintro.lib.grpc.Balance;
+import com.mateus.cardoso.course.grpc.grpcintro.lib.grpc.BalanceCheckRequest;
+import com.mateus.cardoso.course.grpc.grpcintro.lib.grpc.BankServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+
+    @Override
+    public void getBalance(final BalanceCheckRequest request, final StreamObserver<Balance> responseObserver) {
+        final var accountNumber = request.getAccountNumber();
+        final var balance = Balance.newBuilder()
+            .setAmount(accountNumber * 10)
+            .build();
+
+        responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+}
